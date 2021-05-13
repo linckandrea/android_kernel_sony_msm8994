e8ec580ce60945a70e36f7ba926dcc961164099a
Andrea <linckandrea01@gmail.com>
2021-05-13 21:59:10 +0200

    add a compiler script

--- compile_script.sh
+++ compile_script.sh
@@ -6,3 +6,10 @@ make O=out mrproper
 export CROSS_COMPILE=/home/andrea/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 make O=out kitakami_sumire_defconfig
 make O=out -j$(nproc --all)
+
+echo
+echo "Making anykernel zip"
+echo
+cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3/
+cd ./AnyKernel3
+zip -r9 Pop_kernel-sumire-R-rx-x.zip * -x .git README.md *placeholder
